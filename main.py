859168e4eec8a61c342a49ec90e7b047e73ed0fc
Dominic <colter@cua.edu>
2023-08-05 20:33:43 -0400

    File save

--- main.py
+++ main.py
@@ -14,42 +14,65 @@ def validSequence(sequence):
             return False
     return True
 
+def click_button():
+        st.session_state.clicked = True
+
 st.title("DNA Statistics")
-raw_sequence = st.text_input("Enter a DNA sequence with a length of at least 10 nucleotides.")
-space_sequence = raw_sequence.replace(" ", "")
+raw_sequence = st.text_area("Enter a DNA sequence with a length of at least 10 nucleotides.")
+formated_seq = raw_sequence.replace(" ", "").replace("\n", "").upper()
 isValid = False
 
 if raw_sequence != "":
-    if validSequence(space_sequence) == False or len(space_sequence) < 10:
+    if validSequence(formated_seq) == False or len(formated_seq) < 10:
         st.write("Not a valid sequence!")
         isValid = False
     else:
-        st.write("Valid sequence!")
         isValid = True
 
+
 if isValid:
-    final_sequence = Seq(space_sequence)
+    final_sequence = Seq(formated_seq)
     st.write("GC%:", 100 * gc_fraction(final_sequence))
+
+    counts = {"A": final_sequence.count("A"), "T": final_sequence.count("T"), "C": final_sequence.count("C"), "G": final_sequence.count("G"), "Total": len(final_sequence)}
+
+    st.text("Nucleotide Counts: \n" + "A: " 
+    + str(counts["A"]) + "\nT: "
+    + str(counts["T"]) + "\nC: "
+    + str(counts["C"]) + "\nG: "
+    + str(counts["G"]) + "\nTotal: "
+    + str(counts["Total"]))
+
     st.write("Reverse Complement:", final_sequence.reverse_complement())
     st.write("Transcript:", final_sequence.transcribe())
-    st.text("Nucleotide Counts: \n" + "A: " 
-    + str(final_sequence.count("A")) + "\nT: "
-    + str(final_sequence.count("T")) + "\nC: "
-    + str(final_sequence.count("C")) + "\nG: "
-    + str(final_sequence.count("G")) + "\nTotal: "
-    + str(len(space_sequence)))
 
-st.header("Create a Text File with Sequence")
-new_file = st.text_input("File name")
-f = open(new_file + ".txt", "w")
-f.write(str(space_sequence))
-f.close()
-# with File.as_handle(file.name) as fp:
-#     for record in SeqIO.parse(file.name, file_split[1][1:]):
-#         print(record.id)
-#     fp.close()
+    st.write("Amino Acid Sequence: ", final_sequence.translate())
+    st.text("Using the standard genetic code.")
+    st.text(CodonTable.unambiguous_dna_by_id[1])
+    
+    st.header("Save DNA Statistics")
+    new_file = st.text_input("File name")
 
-# print(messenger_rna.translate(table="Vertebrate Mitochondrial"))
+    st.button("Create file", on_click=click_button)
 
-# standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
-# mito_table = CodonTable.unambiguous_dna_by_name["Vertebrate Mitochondrial"]
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+    
+    if new_file != "":
+        if st.session_state.clicked:
+            f = open(new_file + ".txt", "w")
+            f.write("DNA Sequence: \n" + str(final_sequence) + "\n\n")
+            
+            f.write("Nucleotide Count:\n")
+            for nuc, count in counts.items():
+                f.write("%s:%s\n" % (nuc, count))
+            
+            f.write("\nReverse Complement: \n" + str(final_sequence.reverse_complement()) + "\n")
+            f.write("\nTranscript: \n" + str(final_sequence.transcribe()) + "\n")
+            f.write("\nAmino Acid Sequence: \n" + str(final_sequence.transcribe()) + "\n")
+            
+            f.close()
+
+            st.write("Created " + new_file + ".txt")
+    if new_file == "" and st.session_state.clicked:
+        st.write("No file name!")
